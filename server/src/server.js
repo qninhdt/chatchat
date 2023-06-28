95a883e2ec40eba723408c24a4597c5e43ebe28a
qninhdt <qndt123@gmail.com>
2023-06-28 15:35:30 +0700

    remove "urlencoded"

--- server/src/server.js
+++ server/src/server.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
 app.use(cors());
 
 const route = express.Router();
